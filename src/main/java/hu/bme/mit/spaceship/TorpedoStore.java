7a89af6dbb1e1b31bf070f539163bfdbe03bae37
patrikpasztor <47702363+patrikpasztor@users.noreply.github.com>
2020-05-07 19:18:02 +0200

    added comment to Random generator

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,6 +14,7 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
+  // Generating a Random object
   private Random generator = new Random();
 
   public TorpedoStore(int numberOfTorpedos){
